f5b70d3f32d143fda9910f0518454a337a3aac7f
s3845230 <s3845230@student.rmit.edu.au>
2021-10-22 23:32:03 +1100

    framework for command line arguements

---
+++ src/com/s3845230/Main.java
@@ -0,0 +1,14 @@
+package com.s3845230;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.exit(1);
+        }
+        System.out.println(args[0]);
+        if (args[0].equals("list")) {
+
+        }
+    }
+}
